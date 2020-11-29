04873f6feddd3b4db5fb04c0348f941923e3fd67
Antti Mielonen <antti.mielonen@tuni.fi>
2020-11-29 17:32:27 +0200

    test.filter.js

--- test/test.filter.js
+++ test/test.filter.js
@@ -9,8 +9,17 @@ const users = [{ 'user': 'barney', 'active': true },
 
 describe('filter', () => {
     describe('filter', () => {
+        it('sallittu operaatio, filtteri löytyy', () => {
+            expect(filter(users, ({ active }) => active)).to.eql([ { user: 'barney', active: true } ]);
+        })
+        it('sallittu operaatio, filtteriä ei löydy', () => {
+            expect(filter(users, ({ passive }) => passive)).to.eql([[]]);
+        })
         it('sallittu operaatio', () => {
-            expect(filter(users,({ active }))).to.equal(['barney']);
+            expect(filter(users, ({ passive }) => passive)).to.eql([[]]);
+        })
+        it('filtteri puuttuu', () => {
+            expect(filter(users)).to.eql([[]]);
         })
     })
 })

8257a1deaacdfd4213a5661eed5f0456338061d2
Antti Mielonen <antti.mielonen@tuni.fi>
2020-11-29 15:07:38 +0200

    test.chuk.js

--- test/test.chunk.js
+++ test/test.chunk.js
@@ -6,10 +6,13 @@ import chunk from "../src/chunk.js";
 describe('chunk', () => {
     describe('chunk', () => {
         it('sallittu taulukko / 2', () => {
-            expect(chunk(['a', 'b', 'c', 'd'], 2)).to.equal([['a', 'b'], ['c', 'd']])
+            expect(chunk(['a', 'b', 'c', 'd'], 2)).to.eql([['a', 'b'], ['c', 'd']])
         })
         it('sallittu taulukko / 3', () => {
-            expect(chunk(['a', 'b', 'c', 'd'], 3)).to.equal([['a', 'b', 'c'], ['d']])
+            expect(chunk(['a', 'b', 'c', 'd'], 3)).to.eql([['a', 'b', 'c'], ['d']])
+        })
+        it('tyhjä taulukko', () => {
+            expect(chunk([], 3)).to.eql([])
         })
     })
 })

a8ac85e6d7db83d8b824f83e149d2caba767beee
Antti Mielonen <antti.mielonen@tuni.fi>
2020-11-29 16:58:30 +0200

    test.endsWith.js

--- test/test.endsWith.js
+++ test/test.endsWith.js
@@ -11,6 +11,12 @@ describe('endsWith', () => {
         it('sallittu operaatio', () => {
             expect(endsWith('abc', 'b',2)).to.eql(true)
         })
+        it('positio on nolla', () => {
+            expect(endsWith('abc', 'b',0)).to.eql(false)
+        })
+        it('positio on > merkkijonon pituus', () => {
+            expect(endsWith('abc', 'b',5)).to.eql(false)
+        })
         it('numeroita', () => {
             expect(endsWith(123, 3)).to.eql(true)
         })

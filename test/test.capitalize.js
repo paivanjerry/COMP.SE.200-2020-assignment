ce7c057592fa704d1db80b084e0b37b4e925475b
Antti Mielonen <antti.mielonen@tuni.fi>
2020-11-29 12:56:09 +0200

    test.capitalize.js

--- test/test.capitalize.js
+++ test/test.capitalize.js
@@ -3,13 +3,30 @@ const expect = chai.expect
 
 import capitalize from "../src/capitalize.js";
 
-let taulukko = new Uint8Array()
+let pitka = 'FoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobar' +
+    'FoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobar' +
+    'FoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobar' +
+    'FoobarFoobarFoobarFoobarFoobarFoobarFoobarFoobar'
+
+let pitkaCapitalize = 'Foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar' +
+    'foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar' +
+    'foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar' +
+    'foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar'
 
 
 describe('capitalize', () => {
     describe('capitalize', () => {
         it('sallittu merkkijono', () => {
-            expect(capitalize("ABCDEF")).to.equal('Abcdef')
+            expect(capitalize('ABCDEF')).to.equal('Abcdef')
+        })
+        it('pitkä merkkijono > 256 merkkiä', () => {
+            expect(capitalize(pitka)).to.equal(pitkaCapitalize)
+        })
+        it('pelkkiä numeroita', () => {
+            expect(capitalize('12345')).to.equal('12345')
+        })
+        it('ei parametreja / tyhjä', () => {
+            expect(capitalize('')).to.equal('')
         })
     })
 })
